8162b5a3b2918d6f80acb15eb43861ef069785ea
アイ <duckduckgostop@gmail.com>
2026-07-17 00:24:26 +0900

    Initialize Party HUD and HUD editor

--- src/client/java/untitled/untitled/client/UntitledClient.java
+++ src/client/java/untitled/untitled/client/UntitledClient.java
@@ -3,9 +3,10 @@ package untitled.untitled.client;
 import net.fabricmc.api.ClientModInitializer;
 
 public class UntitledClient implements ClientModInitializer {
-
     @Override
     public void onInitializeClient() {
+        EditHud.init();
         ContentTimer.init();
+        PartyHud.init();
     }
 }
