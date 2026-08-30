e2e4fb1c6aacd19ac313a03ef7696e572af9554d
アイ <duckduckgostop@gmail.com>
2026-08-30 15:33:51 +0900

    Initialize only EditHud and PartyHud

--- src/client/java/untitled/untitled/client/UntitledClient.java
+++ src/client/java/untitled/untitled/client/UntitledClient.java
@@ -6,10 +6,6 @@ public class UntitledClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EditHud.register();
-        ContentTimer.init();
         PartyHud.init();
-        CameraControl.init();
-        FoodStack.init();
-        NewSwap.init();
     }
 }
